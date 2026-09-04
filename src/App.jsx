c2d43e0808c4bc7b13e19f8ef405559577eb6476
ahmed-tarek <c.tarek2015@gmail.com>
2026-09-04 22:20:46 +0300

    Add routing and mailbox components; update styles and dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
 import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Navbar.jsx";
+import MailboxList from "./components/MailboxList.jsx";
+import NewMailbox from "./components/NewMailbox.jsx";
+import MailboxDetails from "./components/MailboxDetails.jsx";
 
 function App() {
+  const [mailboxes, setMailboxes] = useState([]);
   return (
     <>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <main>
+              <h1>Post Office</h1>
+            </main>
+          }
+        />
+        <Route
+          path="/mailboxes"
+          element={<MailboxList mailboxes={mailboxes} />}
+        />
+        <Route path="/new-mailbox" element={<NewMailbox />} />
+        <Route path="/mailboxes/:mailboxId" element={<MailboxDetails />} />
+        <Route
+          path="*"
+          element={
+            <main>
+              <h1>404 Not Found</h1>
+            </main>
+          }
+        />
+      </Routes>
       <h1>heeeeeeeey</h1>
     </>
   );
